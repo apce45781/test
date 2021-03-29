f7582d25243e166077c547442c36aa88b382ba79
Arlin <apce45781@gmail.com>
2021-03-29 21:52:43 +0800

    .

--- test.java
+++ test.java
@@ -1,5 +1,14 @@
 public class test {
     public static void main(String[] args) {
-        System.out.println("test_git");
+        int a = 4158964;
+        String dd = "yrdyrdyrdyrdyrd";
+        System.out.println(a + ":test_git:" + dd);
+        new test_class().test1();
+    }
+}
+
+class test_class {
+    public void test1() {
+        System.out.println("test1");
     }
 }
